22d064ac7e118682e8ed54adcd2af9f30e65f908
aman10107 <89832967+aman10107@users.noreply.github.com>
2025-02-09 14:58:18 +0530

    added otp to registration API

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -5,6 +5,8 @@ from rest_framework import serializers
 
 from .models import SendOTPModel
 from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
+
 class SendOTPModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = SendOTPModel
@@ -13,12 +15,19 @@ class SendOTPModelSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
     phone_no = serializers.CharField(write_only=True, min_length=10) 
+    otp = serializers.CharField(write_only=True, min_length=6) 
     class Meta:
         model = User
-        fields = ['password', 'phone_no']
+        fields = ['password', 'phone_no', 'otp']
 
     def validate(self, data):
         data['username'] = data['phone_no']
+        otp = data['otp']
+        phone_number = data['phone_no']
+        otpRecord = SendOTPModel.objects.filter(phone_number=phone_number, otp=otp)
+        if not otpRecord.exists():
+            raise ValidationError("Invalid OTP!")
+        otpRecord.delete()
         return data
 
     def create(self, validated_data):
