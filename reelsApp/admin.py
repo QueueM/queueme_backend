7220a7203cdc837e24d9cb6b069ffb75e59e1ddc
aman10107 <89832967+aman10107@users.noreply.github.com>
2025-03-03 12:44:02 +0530

    added story model to admin panel

--- reelsApp/admin.py
+++ reelsApp/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import ReelsModel, CommentsModel
+from .models import ReelsModel, CommentsModel, StoryModel
 
 admin.site.register(ReelsModel)
 admin.site.register(CommentsModel)
+admin.site.register(StoryModel)
