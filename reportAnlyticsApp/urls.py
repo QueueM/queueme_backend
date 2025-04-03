4fe119a38acd41c594efba43297f6c43ebaf18ef
Tuhin <www.mdtuhin456gmail.com>
2025-04-03 22:06:01 +0600

    fixed linting

--- reportAnlyticsApp/urls.py
+++ reportAnlyticsApp/urls.py
@@ -1,15 +1,12 @@
 from  rest_framework.routers import DefaultRouter 
 from django.urls import path, include
-from .views import(ShopReportApiView ,ServiceReportApiView ,ServiceBookingReportApiView,EmployeeReportApiView)
+from .views import (ShopReportApiView, ServiceReportApiView, ServiceBookingReportApiView, EmployeeReportApiView)
 
 # Initialize the router
 router = DefaultRouter()
 
 # # Register the viewset with the router
 # router.register("customer", CustomersReportApiViewSet, basename="customer_report")
-  
-
-
 urlpatterns = [
     path("", include(router.urls)) ,
     path("shop-report/", ShopReportApiView.as_view(), name="shop_report"),
